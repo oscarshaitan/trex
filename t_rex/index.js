25f8b98d7e9907e5287a5bc22be2b2e4e35b0ec9
Oscar Tigreros <Oscar@symterra.co.uk>
2023-05-16 14:18:29 +0100

    Update index.js

--- t_rex/index.js
+++ t_rex/index.js
@@ -289,13 +289,8 @@
          * definition.
          */
         loadImages: function () {
-            if (IS_HIDPI) {
-                Runner.imageSprite = document.getElementById('offline-resources-2x');
+             Runner.imageSprite = document.getElementById('offline-resources-2x');
                 this.spriteDef = Runner.spriteDefinition.HDPI;
-            } else {
-                Runner.imageSprite = document.getElementById('offline-resources-1x');
-                this.spriteDef = Runner.spriteDefinition.LDPI;
-            }
 
             if (Runner.imageSprite.complete) {
                 this.init();
